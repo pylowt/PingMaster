b4a7acf418d78080f7fef7e868ec2272ced2e1f8
Paul <paul.lowther01@hotmail.com>
2025-05-04 22:20:13 +0100

    Add test for 400 ping

--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -7,3 +7,9 @@ from app.monitor import ping
 async def test_ping():
     status = await ping("https://www.paullowther.dev")
     assert 200 == status
+
+
+@pytest.mark.asyncio
+async def test_ping_400():
+    status = await ping("https://httpstat.us/400")
+    assert 400 == status
