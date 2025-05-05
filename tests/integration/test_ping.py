29b0ece9151f7e80c83119273a39ad1f656d44ae
Paul <paul.lowther01@hotmail.com>
2025-05-05 11:15:09 +0100

    Refactor tests to class & add ci_server fixture startup & teardown

--- tests/integration/test_ping.py
+++ tests/integration/test_ping.py
@@ -1,4 +1,7 @@
 import os
+import subprocess
+import time
+
 import httpx
 import pytest
 
@@ -7,19 +10,35 @@ from app.monitor import ping
 BASE_URL: str = os.getenv("BASE_URL", "http://localhost:8000")
 
 
-@pytest.mark.asyncio
-async def test_ping_200():
-    status = await ping(f"{BASE_URL}/ok")
-    assert 200 == status
+@pytest.fixture(scope="class")
+def ci_server():
+    # Setup a fastapi test server with endpoints returning various status codes
+    proc = subprocess.Popen(
+        ["poetry", "run", "uvicorn", "tests.ci_server:app", "--port", "8000"]
+    )
+    print("running setup")
+    # Crude method for ensuring the server is ready.
+    # TODO: investigate a method to probe the server to confirm startup negating the time.sleep method
+    time.sleep(1)
+    yield
+    print("running teardown")
+    proc.terminate()
 
 
-@pytest.mark.asyncio
-async def test_ping_400():
-    status = await ping(f"{BASE_URL}/bad")
-    assert 400 == status
+@pytest.mark.usefixtures("ci_server")
+class TestPingEndpoints:
 
+    @pytest.mark.asyncio
+    async def test_ping_200(self):
+        status = await ping(f"{BASE_URL}/ok")
+        assert 200 == status
 
-@pytest.mark.asyncio
-async def test_timeout():
-    with pytest.raises(httpx.TimeoutException):
-        await ping(f"{BASE_URL}/timeout", timeout=0.2)
+    @pytest.mark.asyncio
+    async def test_ping_400(self):
+        status = await ping(f"{BASE_URL}/bad")
+        assert 400 == status
+
+    @pytest.mark.asyncio
+    async def test_timeout(self):
+        with pytest.raises(httpx.TimeoutException):
+            await ping(f"{BASE_URL}/timeout", timeout=0.2)
