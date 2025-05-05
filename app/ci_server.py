e97d43c3fb0fbe5dcfbf7042b32611e6d804b64e
Paul <paul.lowther01@hotmail.com>
2025-05-05 09:49:30 +0100

    Add timeout route

--- app/ci_server.py
+++ app/ci_server.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from fastapi import FastAPI
 from fastapi import status
 from fastapi.responses import Response
@@ -12,4 +14,11 @@ async def ok():
 
 @app.get("/bad")
 async def bad():
+    return Response(status_code=status.HTTP_400_BAD_REQUEST)
+
+
+@app.get("/timeout")
+async def timeout():
+    # Ping function must be invoked with a timeout < 0.5s
+    await asyncio.sleep(0.5)
     return Response(status_code=status.HTTP_400_BAD_REQUEST)
